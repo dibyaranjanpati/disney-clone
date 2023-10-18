a80d89d4e2b515193fa0f569017a66c9f5906f89
Dibya <dibyaranjanpati99@gmail.com>
2023-10-19 00:26:57 +0530

    clear

--- src/components/Anything.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Anything = () => {
-  return <div className="absolute top-0 left-0">Anything</div>;
-};
-
-export default Anything;
-
-// this conponents not require
